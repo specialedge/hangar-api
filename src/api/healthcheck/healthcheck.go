12a51cf759aba7d16146fe76ce6e58b52070eb80
Stuart Davidson <github@spedge.com>
2017-11-10 20:31:28 +0000

    Understanding mux
    Figuring out my first proper unit test in mux and how it parses the URL

--- src/api/healthcheck/healthcheck.go
+++ src/api/healthcheck/healthcheck.go
@@ -1,18 +1,14 @@
-package api
+package healthcheck
 
 import (
 	"io"
 	"net/http"
 )
 
-func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+// HandlerHealthcheck : A healthcheck to confirm the underlying system is performing as expected.
+func HandlerHealthcheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 
 	io.WriteString(w, `{"alive": true}`)
-}
-
-func main() {
-	http.HandleFunc("/health-check", healthcheckHandler)
-	http.ListenAndServe(":8080", nil)
 }
