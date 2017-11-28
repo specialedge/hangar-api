3abc026f7fdff454b351d3548cb4204fbb9c4223
Stuart Davidson <github@spedge.com>
2017-11-28 14:22:17 +0000

    Making grab work initially

---
+++ src/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"./api/healthcheck"
+	"./api/java"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// Initial admin & service endpoints
+	r.HandleFunc("/healthcheck", healthcheck.HandlerHealthcheck)
+
+	// Java Endpoints
+	java.AppendJavaDownloadMetadataRouter(r)
+
+	http.ListenAndServe(":8080", r)
+}
